c8b188d8a9c17030e5bcd7c2f5dadfcc92bb5544
Clara Gyökeres <you@example.com>
2020-03-14 13:53:18 +0100

    created a store function

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    products: []
   },
   mutations: {
+    addItem(state, product) {
+      state.products.push(product)
+    }
   },
   actions: {
+    addItem({ commit, state }, product) {
+      commit('addItem', product)
+    }
   },
   modules: {
   }
