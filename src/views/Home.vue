e28d00216122fed7d7eced51486494a0711c29e8
Clara Gyökeres <you@example.com>
2020-03-14 12:33:44 +0100

    updated the thumnails

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
     <h1>This is my Shopping cart</h1>
+    <div class="productlist">
+      <article v-for="product in products" v-bind:key="product.id" class="product">
+        <img class="thumbnail" :src="product.thumbnail" />
+        <p class="product-title">{{product.name}}</p>
+        <p class="product-price">{{product.price}}:-</p>
+      </article>
+    </div>
   </div>
 </template>
 
 <script>
+import data from "@/data/products.json";
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      products: data
+    };
+  },
+  mounted() {}
 };
 </script>
